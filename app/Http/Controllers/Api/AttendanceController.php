77b58e22343b1d7109dcf0d453b6ffcad2047171
Snow Deep <barondeepsnow@gmail.com>
2023-08-03 14:17:08 +0700

    Shift Access - made by Sweetsica

---
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Member;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Nette\Utils\Random;
+use Faker\Factory;
+
+class AttendanceController extends Controller
+{
+
+    public function action(Request $request, $function =0, $userId=0, $info=0)
+    {
+        dump($function);
+        dump($userId);
+        dump($info);
+        dd('done');
+    }
+
+    public function home()
+    {
+        $member = Member::take(5)->orderBy('id', 'desc')->get();
+        return view ('home',compact('member'));
+    }
+
+    public function createRandom()
+    {
+        $faker = Factory::create();
+        $member = Member::create([
+            'name' => $faker->name,
+            'token' => Str::random('5'),
+            'departmentId' => $faker->unique()->numberBetween(0,9)
+        ]);
+        $member->save;
+
+        return redirect()->route('home');
+    }
+}
